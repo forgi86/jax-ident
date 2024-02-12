cba09247f3e491bc35aa15dfd6db49795ae76663
Marco Forgione <marco.forgione1986@gmail.com>
2024-02-12 15:34:51 +0100

    code reformatted

--- train.py
+++ train.py
@@ -12,21 +12,20 @@ from jaxid.models import StateUpdateAndOptput, MLP
 import nonlinear_benchmarks
 
 
-
 # Configuration
 cfg = {
-
     # Training
     "batch_size": 32,
     "seq_len": 1000,
     "skip": 100,
     "lr": 2e-4,
     "epochs": 10,
-
     # Model
     "nx": 10,
     "ny": 1,
     "nu": 1,
+    "hidden_f": [32, 16],
+    "hidden_g": [32, 16],
 }
 cfg = Namespace(**cfg)
 
@@ -36,7 +35,7 @@ key = random.key(42)
 # Load data
 train_val, test = nonlinear_benchmarks.WienerHammerBenchMark()
 sampling_time = train_val.sampling_time
-u_train, y_train = train_val   
+u_train, y_train = train_val
 u_train = u_train.reshape(-1, cfg.nu)
 y_train = y_train.reshape(-1, cfg.ny)
 
@@ -49,13 +48,15 @@ scaler_y = StandardScaler()
 y = scaler_y.fit_transform(y_train)
 
 # Make Dataset and Dataloaders
-train_data = SubsequenceDataset(u, y, subseq_len=cfg.seq_len)  
-train_loader = NumpyLoader(train_data, batch_size=cfg.batch_size, shuffle=True, drop_last=True)
+train_data = SubsequenceDataset(u, y, subseq_len=cfg.seq_len)
+train_loader = NumpyLoader(
+    train_data, batch_size=cfg.batch_size, shuffle=True, drop_last=True
+)
 
 
 # Make model
-f_xu = MLP([32, 16, cfg.nx])
-g_x = MLP([32, 16, cfg.ny])
+f_xu = MLP(cfg.hidden_f + [cfg.nx])
+g_x = MLP(cfg.hidden_g + [cfg.ny])
 fg = StateUpdateAndOptput(f_xu, g_x)
 
 # Initialize model parameters
@@ -64,19 +65,22 @@ u = jnp.ones((cfg.nu,))
 key, subkey = random.split(key)
 params = fg.init(subkey, x, u)
 
+
 # Define loss
 def simulate(params, x0, u_seq):
     fg_func = lambda x, u: fg.apply(params, x, u)
     return jax.lax.scan(fg_func, x0, u_seq)
 
+
 def loss_fn(params, batch_x0, batch_u, batch_y):
     def sequence_mse(x0, u_seq, y_seq):
         _, y_hat = simulate(params, x0, u_seq)
-        err = y_seq[cfg.skip:] - y_hat[cfg.skip:]
+        err = y_seq[cfg.skip :] - y_hat[cfg.skip :]
         loss = jnp.mean(err**2)
         return loss
-    
+
     return jnp.mean(jax.vmap(sequence_mse)(batch_x0, batch_u, batch_y), axis=0)
+
 
 # Setup optimizer
 optimizer = optax.adam(learning_rate=cfg.lr)
@@ -92,19 +96,21 @@ for epoch in range(cfg.epochs):
         updates, opt_state = optimizer.update(grads, opt_state)
         params = optax.apply_updates(params, updates)
         LOSS.append(loss_val)
-        if  idx % 100 == 0:
-                print('Loss step {}: '.format(idx), loss_val)
+        if idx % 100 == 0:
+            print("Loss step {}: ".format(idx), loss_val)
 
 
 # Save a checkpoint
-ckpt = {'params': params,
-        'cfg': vars(cfg),
-        'LOSS': jnp.array(LOSS)
-        }
-        #'scaler_u': scaler_u,
-        #'scaler_y': scaler_y} # cannot save these python objects unfortunately...
+ckpt = {
+    "params": params,
+    "cfg": vars(cfg),
+    "LOSS": jnp.array(LOSS),
+    "scaler_u": scaler_u,
+    "scaler_y": scaler_y,
+}  # scalers are not correctly saved unfortunately...
 
 orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
 save_args = orbax_utils.save_args_from_target(ckpt)
-orbax_checkpointer.save(Path(".").resolve()/ "models" / "model1", ckpt, save_args=save_args)
-
+orbax_checkpointer.save(
+    Path(".").resolve() / "models" / "model1", ckpt, save_args=save_args, force=True
+)
